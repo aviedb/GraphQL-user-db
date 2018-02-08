a3bd77cfae1865970551630a86fe65fd7c8535a3
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2018-02-08 12:07:06 +0700

    Add mutation addUser

--- schema/schema.js
+++ schema/schema.js
@@ -2,7 +2,7 @@ const graphql = require('graphql')
 const axios = require('axios')
 // const _ = require('lodash')
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList, GraphQLNonNull } = graphql
 
 // const users = [
 //   { id: '23', firstName: 'Avied', age: 17 },
@@ -59,6 +59,24 @@ const RootQuery = new GraphQLObjectType({
   }
 })
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+        companyId: { type: GraphQLString }
+      },
+      resolve(parentValue, { firstName, age }) {
+        return axios.post('http://localhost:3000/users', { firstName, age }).then((resp) => resp.data)
+      }
+    }
+  }
+})
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation
 })
